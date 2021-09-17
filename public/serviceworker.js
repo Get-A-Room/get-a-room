bcb49f44b1192e3bfb2e145fd01c6749c627f69c
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-09-17 15:17:13 +0300

    Add and apply prettier styling

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,29 +1,28 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html', 'offline.html'];
 
 const self = this;
 
 // Install SW
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
 
-                return cache.addAll(urlsToCache);
-            })
-    )
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 // Listen for requests
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request) 
-                    .catch(() => caches.match('offline.html'))
-            })
-    )
+        caches.match(event.request).then(() => {
+            return fetch(event.request).catch(() =>
+                caches.match('offline.html')
+            );
+        })
+    );
 });
 
 // Activate the SW
@@ -32,13 +31,15 @@ self.addEventListener('activate', (event) => {
     cacheWhitelist.push(CACHE_NAME);
 
     event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-            
-    )
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                    return null;
+                })
+            )
+        )
+    );
 });
