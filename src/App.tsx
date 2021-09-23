267a59a9f3bd1efc33af4b3709e33568e514d7c9
Joona Prehti <joona.prehti@tuni.fi>
2021-09-23 15:23:22 +0300

    Little change to HTML to see if changes really happen

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Get A Room!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     );
