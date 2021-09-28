a62690771ae9705cf87837d88857aff5190f42d5
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-09-28 16:47:37 +0300

    Separate frontend and backend folders

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Get A Room Testaan Pipelinen toimivuutta!</p>
+                <p>Get A Room Testaan Pipelinen toimivuutta!!</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
