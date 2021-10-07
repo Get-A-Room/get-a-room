9ddcf0c53c7710f7ef2a14610baf595dcc1dc100
Martti Grönholm <martti.gronholm@tuni.fi>
2021-10-07 13:18:13 +0300

    Add structure for BookingView page. Minor modifications to other files.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import logo from './../logo.svg';
 import './App.css';
+import BookingView from './BookingView'
+import AppBar from './AppBar'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Get A Room Testaan Pipelinen toimivuutta!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React!
-                </a>
-            </header>
+            <BookingView />
         </div>
     );
 }
---
+++ frontend/src/components/BookingView.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function BookingView() {
+    return (
+        <div className="BookingView">
+            <header className="BookingView-header">
+                <h1>Available rooms</h1>
+            </header>
+            <body>
+                <p>Room1 <Button>Book</Button></p>
+                <p>Room2 <Button>Book</Button></p>
+                <p>Room3 <Button>Book</Button></p>
+            </body>
+        </div>
+    );
+}
+
+export default BookingView;
