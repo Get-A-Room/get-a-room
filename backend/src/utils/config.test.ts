bab7bf4a75a40daad32ad756c1827f73b5cd1d8d
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-10-08 12:51:47 +0300

    Move database url to env variable

--- backend/src/utils/config.test.ts
+++ backend/src/utils/config.test.ts
@@ -3,37 +3,43 @@ import { createDatabaseUrl, DEV_DB_NAME, PROD_DB_NAME } from './config';
 describe('createDatabaseUrl', () => {
     test('Should fail without db user', () => {
         expect(() => {
-            createDatabaseUrl(false, undefined, 'password');
-        }).toThrow('credentials');
+            createDatabaseUrl(undefined, 'password', 'dbUrl', false);
+        }).toThrow('user');
     });
 
     test('Should fail without db password', () => {
         expect(() => {
-            createDatabaseUrl(false, 'username', undefined);
-        }).toThrow('credentials');
+            createDatabaseUrl('username', undefined, 'dbUrl', false);
+        }).toThrow('password');
     });
 
-    test('Should create url containing username:password', () => {
-        expect(createDatabaseUrl(false, 'username', 'password')).toContain(
-            'username:password'
-        );
+    test('Should fail without db url', () => {
+        expect(() => {
+            createDatabaseUrl('username', 'password', undefined, false);
+        }).toThrow('url');
+    });
+
+    test('Should create url containing username:password@dbUrl', () => {
+        expect(
+            createDatabaseUrl('username', 'password', 'dbUrl', false)
+        ).toContain('username:password@dbUrl');
     });
 
     test('Should use development database by default', () => {
-        expect(createDatabaseUrl(undefined, 'username', 'password')).toContain(
+        expect(createDatabaseUrl('username', 'password', 'dbUrl')).toContain(
             DEV_DB_NAME
         );
     });
 
     test('Should use production database in production environment', () => {
-        expect(createDatabaseUrl(true, 'username', 'password')).toContain(
-            PROD_DB_NAME
-        );
+        expect(
+            createDatabaseUrl('username', 'password', 'dbUrl', true)
+        ).toContain(PROD_DB_NAME);
     });
 
     test('Should use development database in dev environment', () => {
-        expect(createDatabaseUrl(false, 'username', 'password')).toContain(
-            DEV_DB_NAME
-        );
+        expect(
+            createDatabaseUrl('username', 'password', 'dbUrl', false)
+        ).toContain(DEV_DB_NAME);
     });
 });
--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,22 +1,28 @@
-export const DB_URL = 'get-a-room.3b3o5.mongodb.net';
 export const PROD_DB_NAME = 'getARoomDB';
 export const DEV_DB_NAME = 'getARoomDevDB';
 
 export const getDatabaseUrl = () => {
-    const { NODE_ENV, DB_USER, DB_PASSWORD } = process.env;
+    const { NODE_ENV, DB_USER, DB_PASSWORD, DB_URL } = process.env;
     const isProduction = NODE_ENV === 'production';
-    return createDatabaseUrl(isProduction, DB_USER, DB_PASSWORD);
+    return createDatabaseUrl(DB_USER, DB_PASSWORD, DB_URL, isProduction);
 };
 
 export const createDatabaseUrl = (
-    isProduction = false,
     dbUser?: string,
-    dbPassword?: string
+    dbPassword?: string,
+    dbUrl?: string,
+    isProduction = false
 ) => {
-    if (!dbUser || !dbPassword) {
-        throw new Error('Database credentials not defined');
+    if (!dbUser) {
+        throw new Error('Database user not defined');
+    }
+    if (!dbPassword) {
+        throw new Error('Database password not defined');
+    }
+    if (!dbUrl) {
+        throw new Error('Database url not defined');
     }
     const databaseName = isProduction ? PROD_DB_NAME : DEV_DB_NAME;
 
-    return `mongodb+srv://${dbUser}:${dbPassword}@${DB_URL}/${databaseName}?retryWrites=true&w=majority`;
+    return `mongodb+srv://${dbUser}:${dbPassword}@${dbUrl}/${databaseName}?retryWrites=true&w=majority`;
 };
