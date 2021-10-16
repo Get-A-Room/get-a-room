20f3399368350fbbdf668516f926a152f5761c00
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-10-16 22:19:56 +0300

    Implement booking

--- backend/src/controllers/googleAPI/calendarAPI.ts
+++ backend/src/controllers/googleAPI/calendarAPI.ts
@@ -49,3 +49,96 @@ export const freeBusyQuery = async (
         throw err;
     }
 };
+
+/**
+ * Create an event, will not check if the rooms is free
+ * @param client OAuth2Client
+ * @param room Room email
+ * @param organizer Email of the current user
+ * @param title Title of the event
+ * @param start Start of the event
+ * @param end End of the event
+ */
+export const createEvent = async (
+    client: OAuth2Client,
+    room: string,
+    organizer: string,
+    title: string,
+    start: string,
+    end: string
+): Promise<schema.EventData> => {
+    try {
+        const startDt: schema.EventDateTier = {
+            dateTime: start
+        };
+        const endDt: schema.EventDateTier = {
+            dateTime: end
+        };
+
+        const attendeeList: schema.EventAttendee[] = [
+            { email: room, resource: true },
+            { email: organizer, responseStatus: 'accepted' }
+        ];
+
+        const event: schema.EventData = {
+            summary: title,
+            start: startDt,
+            end: endDt,
+            attendees: attendeeList,
+            reminders: {
+                useDefault: false
+            }
+        };
+
+        const eventResult = await calendar.events.insert({
+            calendarId: 'primary',
+            requestBody: event,
+            auth: client
+        });
+
+        return eventResult.data;
+    } catch (err) {
+        throw err;
+    }
+};
+
+/**
+ * Deletes the event of the current client
+ * @param client OAuth2Client
+ * @param eventId Id of the event to delete
+ */
+export const deleteEvent = async (client: OAuth2Client, eventId: string) => {
+    try {
+        await calendar.events.delete({
+            calendarId: 'primary',
+            eventId: eventId,
+            sendUpdates: 'none',
+            auth: client
+        });
+    } catch (err) {
+        throw err;
+    }
+};
+
+/**
+ * Gets response status of the room of the event
+ * @param client OAuth2Client
+ * @param roomId Id of the room (email)
+ * @param eventId Id of the event to lookup
+ */
+export const getEventData = async (
+    client: OAuth2Client,
+    eventId: string
+): Promise<schema.EventData> => {
+    try {
+        const response = await calendar.events.get({
+            calendarId: 'primary',
+            eventId: eventId,
+            auth: client
+        });
+
+        return response.data;
+    } catch (err) {
+        throw err;
+    }
+};
