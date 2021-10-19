38e3a4b382eb8d535b042905cded6cb2523835b6
Martti Grönholm <martti.gronholm@tuni.fi>
2021-10-19 14:31:44 +0300

    Fetch and display rooms data in BookingView. Style components.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,7 +3,6 @@ import logo from './../logo.svg';
 import './App.css';
 import Header from './Header'
 import MainView from './MainView'
-import BookingView from './BookingView'
 
 function App() {
     return (
--- frontend/src/components/BookingView.tsx
+++ frontend/src/components/BookingView.tsx
@@ -1,53 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
+import { useState, useEffect, Key, ReactChild, ReactFragment, ReactPortal } from 'react';
+import {
+    Button,
+    Box,
+    List,
+    ListItem,
+    ListItemText
+} from '@mui/material';
+import './BookingView.css'
+
 
 function BookingView() {
-    const token = 'ya29.a0ARrdaM_IYvLuNV3qX6vTJHKfn-jH9ARt3uUpWJLHk-yYJp0VdHwG-pk6pjOc9OJ6GWYzJUR7xjefMof_LNzHwjkyQQ' +
-        'ETjMKUfdxNZUxL_rITosJTSPkdjseUqafFcN40LukiFyAxD7ArAJbLwA95LxYC1g9Vzw';
+    const token = 'ya29.a0ARrdaM-bnoXH7SMpZIUk1DKMmGvk2wOjpKi9am6LjcJUeqxjA7bGILaUybviAyE-RSdDTTIMWdQjd5wsWGaDGk0YFy06qVjn3Shn63T1hKNagUUOqCgQ9WHdRrbZp70QkdtM9M4ENDWhXQSoJKRVWMzuBlD8Sg';
 
     const backendUrl = 'http://localhost:8080';
 
-    const [rooms, setRooms] = useState([]);
+    const [rooms, setRooms] = useState<any[]>([]);
 
     useEffect(() => {
         getRooms();
 
         async function getRooms() {
-            const res = await fetch(backendUrl + '/rooms');
+            const res = await fetch(backendUrl + '/rooms', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json',
+                }
+            });
             const room = await res.json();
 
             setRooms(room);
         }
     }, []);
 
-    /*
-    fetch(backendUrl + '/rooms', {
-        method: 'GET',
-        headers: {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-        }
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-    */
+    // Rooms as an array
+    let arrayOfRooms: any = rooms['rooms' as any];
 
-    // TODO: Display the rooms
     return (
         <div className="BookingView">
             <header className="BookingView-header">
                 <h1>Available rooms</h1>
             </header>
-            {rooms && (
-                <div className="rooms">
-                    {rooms.map((room, index) => (
-                        <div key={index}>
-                            <h2>{room}</h2>
-                        </div>
-                    ))}
-                </div>
-            )}
-            <Button>Book</Button>
+            <List>
+                    {arrayOfRooms && arrayOfRooms.map((room: { id: Key | null | undefined; name: boolean |
+                                                                ReactChild | ReactFragment | ReactPortal |
+                                                                null | undefined; }) =>
+                        <Box key={room.id} m={1} border={2} borderColor='#282c34'>
+                            <ListItem>
+                                <ListItemText
+                                    disableTypography
+                                    style={{
+                                        fontSize: '18x',
+                                        fontWeight: 'bold',
+                                    }}>
+                                    {room.name}
+                                </ListItemText>
+                                <Button
+                                    style={{
+                                        backgroundColor: '#282c34',
+                                        color: 'white',
+                                        fontSize: '18px',
+                                        textTransform: 'none',
+                                        minWidth: '120px',
+                                        minHeight: '50px',
+                                        maxWidth: '120px',
+                                        maxHeight: '50px'
+                                    }}>
+                                    Quick Book
+                                </Button>
+                            </ListItem>
+                        </Box>
+                    )}
+            </List>
         </div>
     );
 }
