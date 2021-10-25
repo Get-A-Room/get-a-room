93b6d94e35f6f171d1e30b8c27a8c9a94673b9ff
Martti Grönholm <martti.gronholm@tuni.fi>
2021-10-25 15:09:08 +0300

    Remove unused imports.

--- frontend/src/components/MainView.tsx
+++ frontend/src/components/MainView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MainView.css';
-import Login from './GoogleLogin';
-import OfficeSelection from './SelectOffice';
+//import Login from './GoogleLogin';
+//import OfficeSelection from './SelectOffice';
 import BookingView from './BookingView';
 
 const MainView = () => {
