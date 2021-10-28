fb144e6ad679e3e39e4d2e86a16a8dd8e9e88e99
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-10-28 17:22:49 +0300

    Implement logout

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,22 +3,14 @@ import { router as googleRouter } from '../controllers/googleController';
 
 export const router = express.Router();
 
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+
 // Google authentication
 router.use('/google', googleRouter);
 
-// REMOVE THESE WHEN FRONTEND IS READY FOR USE
-router.get('/success', (req: express.Request, res: express.Response) => {
-    if (req.query.token && req.query.name) {
-        res.send(
-            `<h1>Google login successful :)</h1><h3>User: ${req.query.name}</h3><p>Token: ${req.query.token}</p>`
-        );
-    } else {
-        res.send('<h1>Google login successful :)</h1>');
-    }
-});
-
-router.get('/failure', (req: express.Request, res: express.Response) => {
-    res.send(
-        '<h1>Google login failed :-(</h1><a href="/auth/google">Continue</a>'
-    );
+// Log out
+router.use('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.clearCookie('refreshToken');
+    res.redirect(`${frontendUrl}`);
 });
