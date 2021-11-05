61b1e951a0831e0c841e2b339fd9e6884e532c2b
Joona Prehti <joona.prehti@tuni.fi>
2021-11-05 19:27:09 +0200

    Refactored simplifyBookings to own function

--- backend/src/controllers/booking/currentBookingsController.ts
+++ backend/src/controllers/booking/currentBookingsController.ts
@@ -12,82 +12,6 @@ import { simplifyRoomData } from '../../controllers/roomController';
 import roomData from '../../interfaces/roomData';
 
 /**
- * Simplifies and filters current bookings
- * @returns
- */
-export const simplifyCurrentBookingsMiddleware = () => {
-    const middleware = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        try {
-            const allBookings: currentBookingData[] =
-                res.locals.currentBookings.items;
-
-            const rooms: schema.CalendarResource[] = await admin.getRoomData(
-                res.locals.oAuthClient
-            );
-            const roomsSimplified: roomData[] = simplifyRoomData(rooms);
-
-            const simplifiedBookings = allBookings.map(
-                (booking: schema.EventData) => {
-                    const simpleEvent: currentBookingData = {
-                        id: booking.id,
-                        startTime: booking.start?.dateTime,
-                        endTime: booking.end?.dateTime,
-                        room: null
-                    };
-
-                    // Finds the room information and includes it inside the simpleEvent
-                    const room = roomsSimplified.find((room: any) => {
-                        return room.location === booking.location;
-                    });
-                    simpleEvent.room = room;
-
-                    return simpleEvent;
-                }
-            );
-
-            res.locals.currentBookings =
-                filteCurrentlyRunningBookings(simplifiedBookings);
-            next();
-        } catch (err) {
-            next(err);
-        }
-    };
-
-    return middleware;
-};
-
-/**
- * Filters away every booking that is not currently running
- * @param simplifiedBookings List of simplified bookings
- * @returns filtered bookings
- */
-export const filteCurrentlyRunningBookings = (
-    simplifiedBookings: currentBookingData[]
-): currentBookingData[] => {
-    // Filters away all bookings that aren't running at the moment
-    const onlyCurrentlyRunningBookings: currentBookingData[] =
-        simplifiedBookings.filter((booking: currentBookingData) => {
-            if (!booking.startTime || !booking.endTime) {
-                return false;
-            }
-
-            // Checks that the event has a room or other resource
-            if (_.isEmpty(booking.room)) {
-                return false;
-            }
-
-            const now = DateTime.local().toISO();
-            return booking.startTime <= now && booking.endTime >= now;
-        });
-
-    return onlyCurrentlyRunningBookings;
-};
-
-/**
  * Gets all the users currently active bookings
  * @returns
  */
@@ -116,4 +40,94 @@ export const getCurrentBookingsMiddleware = () => {
     };
 
     return middleware;
+};
+
+/**
+ * Simplifies and filters current bookings
+ * @returns
+ */
+export const simplifyCurrentBookingsMiddleware = () => {
+    const middleware = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const allBookings: currentBookingData[] =
+                res.locals.currentBookings.items;
+
+            const rooms: schema.CalendarResource[] = await admin.getRoomData(
+                res.locals.oAuthClient
+            );
+
+            const simplifiedBookings = simplifyBookings(allBookings, rooms);
+
+            res.locals.currentBookings =
+                filteCurrentlyRunningBookings(simplifiedBookings);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+
+    return middleware;
+};
+
+/**
+ * Simlpifies bookings
+ * @param simplifiedBookings List of all bookings
+ * @returns simplified bookings
+ */
+export const simplifyBookings = (
+    allBookings: currentBookingData[],
+    rooms: schema.CalendarResource[]
+): currentBookingData[] => {
+    // Filters away all bookings that aren't running at the moment
+    const roomsSimplified: roomData[] = simplifyRoomData(rooms);
+
+    const simplifiedBookings = allBookings.map((booking: schema.EventData) => {
+        const simpleEvent: currentBookingData = {
+            id: booking.id,
+            startTime: booking.start?.dateTime,
+            endTime: booking.end?.dateTime,
+            room: null
+        };
+
+        // Finds the room information and includes it inside the simpleEvent
+        const room = roomsSimplified.find((room: any) => {
+            return room.location === booking.location;
+        });
+        simpleEvent.room = room;
+
+        return simpleEvent;
+    });
+
+    return simplifiedBookings;
+};
+
+/**
+ * Filters away every booking that is not currently running
+ * @param simplifiedBookings List of simplified bookings
+ * @returns filtered bookings
+ */
+export const filteCurrentlyRunningBookings = (
+    simplifiedBookings: currentBookingData[]
+): currentBookingData[] => {
+    // Filters away all bookings that aren't running at the moment
+    const onlyCurrentlyRunningBookings: currentBookingData[] =
+        simplifiedBookings.filter((booking: currentBookingData) => {
+            if (!booking.startTime || !booking.endTime) {
+                return false;
+            }
+
+            // Checks that the event has a room or other resource
+            if (_.isEmpty(booking.room)) {
+                return false;
+            }
+
+            const now = DateTime.local().toISO();
+            return booking.startTime <= now && booking.endTime >= now;
+        });
+
+    return onlyCurrentlyRunningBookings;
 };
