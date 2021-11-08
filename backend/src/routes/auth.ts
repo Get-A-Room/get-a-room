9873e49634a25ae48fe73a5e883ec16c5c8405dc
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2021-11-08 15:58:59 +0200

    Made logout gettable

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,14 +3,12 @@ import { router as googleRouter } from '../controllers/googleController';
 
 export const router = express.Router();
 
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
-
 // Google authentication
 router.use('/google', googleRouter);
 
 // Log out
-router.use('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
     res.clearCookie('token');
     res.clearCookie('refreshToken');
-    res.redirect(`${frontendUrl}`);
+    res.status(204).send('No Content');
 });
