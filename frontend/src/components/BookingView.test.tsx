b94bf3755d60497a7ef65c8a49ddb6e3cca378d0
Martti Grönholm <martti.gronholm@tuni.fi>
2021-11-08 09:32:59 +0200

    Remove BookingView.test.tsx

--- frontend/src/components/BookingView.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, fireEvent, getByTestId } from '@testing-library/react';
-import BookingView from './BookingView';
-
-test('renders BookingView.tsx', () => {
-    render(<BookingView />);
-    expect(true).toBe(true);
-});
