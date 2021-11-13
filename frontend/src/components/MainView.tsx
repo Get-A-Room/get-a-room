f204942cd48d5f0df1920ace9d96fd5aa398591e
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-11-13 12:54:41 +0200

    Add PreferencesLoader component
    - Shown after login
    - Waits until preferences are loaded and either:
      - If officepreference is set earlier forwards to booking screen
      - Else asks and sets the officepreference

--- frontend/src/components/MainView.tsx
+++ frontend/src/components/MainView.tsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './MainView.css';
-import Login from './GoogleLogin';
 import BookingView from './BookingView';
 import PreferencesView from './PreferencesView';
-import { Preferences } from '../types';
+import { Building, Preferences } from '../types';
 import { getPreferences } from '../services/preferencesService';
 import NavBar from './NavBar';
+import { getBuildings } from '../services/buildingService';
+import PreferencesLoader from './PreferencesLoader';
 
 const MainView = () => {
     const [preferences, setPreferences] = useState<Preferences | undefined>();
 
+    const [buildings, setBuildings] = useState<Building[]>([]);
+
+    useEffect(() => {
+        getBuildings().then(setBuildings);
+    }, []);
+
     useEffect(() => {
         getPreferences()
             .then(setPreferences)
-            .catch((error) => {
-                setPreferences(undefined);
+            .catch(() => {
+                // Redirected to login
             });
     }, []);
 
@@ -27,6 +34,13 @@ const MainView = () => {
                     <PreferencesView
                         preferences={preferences}
                         setPreferences={setPreferences}
+                    />
+                </Route>
+                <Route path="/auth/success">
+                    <PreferencesLoader
+                        preferences={preferences}
+                        setPreferences={setPreferences}
+                        buildings={buildings}
                     />
                 </Route>
                 <Route path="/">
---
+++ frontend/src/components/PreferencesLoader.tsx
@@ -0,0 +1,79 @@
+import { Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+
+import { Building, Preferences } from '../types';
+import { updatePreferences } from '../services/preferencesService';
+import SelectOffice from './SelectOffice';
+import CenteredProgress from './util/CenteredProgress';
+import FormButtons from './util/FormButtons';
+
+type PreferencesLoaderProps = {
+    preferences?: Preferences;
+    setPreferences: (preferences?: Preferences) => any;
+    buildings: Building[];
+};
+
+/**
+ * After preferences loaded, either asks the user for building preference.
+ * If the building is already set (in earlier session), forwards straight to the booking view
+ */
+const PreferencesLoader = (props: PreferencesLoaderProps) => {
+    const { preferences, setPreferences, buildings } = props;
+    const [selectedOffice, setSelectedOffice] = useState('');
+
+    const history = useHistory();
+
+    const goToMainView = () => {
+        // Use replace in place of push because it's a temporary page and wouln't work if navigated back to
+        history.replace('/');
+    };
+
+    const savePreferences = () => {
+        const foundBuilding = buildings.find(
+            (building) => building.id === selectedOffice
+        );
+        if (!foundBuilding) {
+            alert('Error occurred while setting building');
+            return;
+        }
+        updatePreferences({ building: foundBuilding }).then(
+            (savedPreferences) => {
+                setPreferences(savedPreferences);
+                goToMainView();
+            }
+        );
+    };
+
+    if (!preferences) {
+        // Loading preferences
+        return <CenteredProgress />;
+    }
+    if (preferences.building?.id) {
+        // Preferences already set
+        goToMainView();
+        return null;
+    }
+    /* Height propery should maybe be changed to 100% when css is sorted out,
+     so that it can depend on parent component instead of setting its own height */
+    return (
+        <Stack pt={5} justifyContent="space-between" height="80vh">
+            <Typography variant="h4" color="#f04e30" fontWeight="bold">
+                Select your office
+            </Typography>
+            <SelectOffice
+                buildings={buildings}
+                selectedOffice={selectedOffice}
+                setSelectedOffice={setSelectedOffice}
+            />
+            <FormButtons
+                submitText="Confirm"
+                handleSubmit={savePreferences}
+                cancelText="Skip"
+                handleCancel={goToMainView}
+            />
+        </Stack>
+    );
+};
+
+export default PreferencesLoader;
