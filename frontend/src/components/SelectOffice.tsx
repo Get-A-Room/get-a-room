ce2313368e562c3dbb0af56a0e548acc86d1a165
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-11-13 12:48:49 +0200

    Refactor SelectOffice to not hold state
    Now receives current office state from the higher ups.

--- frontend/src/components/SelectOffice.tsx
+++ frontend/src/components/SelectOffice.tsx
@@ -8,52 +8,25 @@ import {
     SelectChangeEvent
 } from '@mui/material';
 import './SelectOffice.css';
-import { getBuildings } from '../services/buildingService';
-import { Building, Preferences } from '../types';
-
-// Check if rooms are fetched
-function areBuildingsFetched(buildings: Building[]) {
-    return Array.isArray(buildings) && buildings.length > 0;
-}
+import { Building } from '../types';
+import { isNonEmptyArray } from '../util/objectUtils';
 
 type SelectOfficeProps = {
-    currentPreferencesOffice?: string;
-    selectedPreferences?: Preferences;
-    setSelectedPreferences: (preferences?: Preferences) => any;
+    selectedOffice: string;
+    setSelectedOffice: (officeId: string) => any;
+    buildings: Building[];
 };
 
 const SelectOffice = (props: SelectOfficeProps) => {
-    const {
-        selectedPreferences,
-        setSelectedPreferences,
-        currentPreferencesOffice
-    } = props;
-    const [buildings, setBuildings] = useState<Building[]>([]);
-    const [selectedOffice, setSelectedOffice] = useState(
-        currentPreferencesOffice || ''
-    );
-
-    useEffect(() => {
-        getBuildings().then(setBuildings);
-    }, []);
+    const { selectedOffice, setSelectedOffice, buildings } = props;
 
     const handleChange = (event: SelectChangeEvent) => {
         // Set select component state
         const buildingId = event.target.value;
         setSelectedOffice(buildingId);
-
-        // if valid building set selected preferences
-        const foundBuilding = buildings.find(
-            (building) => building.id === buildingId
-        );
-        if (foundBuilding) {
-            setSelectedPreferences({
-                ...selectedPreferences,
-                building: foundBuilding
-            });
-        }
     };
 
+    if (!buildings) return null;
     return (
         <div className="OfficeSelect">
             <Box>
@@ -68,7 +41,7 @@ const SelectOffice = (props: SelectOfficeProps) => {
                         id="office-select"
                         label="Office location"
                     >
-                        {areBuildingsFetched(buildings) &&
+                        {isNonEmptyArray(buildings) &&
                             buildings.map((building) => (
                                 <MenuItem key={building.id} value={building.id}>
                                     {building.name}
