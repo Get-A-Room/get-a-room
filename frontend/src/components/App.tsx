f341ae21ae76d72e10fe0e964cb7d97028e84ef5
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-11-19 11:22:09 +0200

    Add initial notification implementation

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 import './App.css';
 import { Route, Router, Switch } from 'react-router-dom';
 import { history } from '../services/axiosConfigurer';
+import { SnackbarProvider } from 'notistack';
 import MainView from './MainView';
 import LoginView from './LoginView';
 
 function App() {
+    console.log(document.getElementById('main-view'));
     return (
-        <Router history={history}>
-            <div className="App">
-                <Switch>
-                    <Route path="/login">
-                        <LoginView />
-                    </Route>
-                    <Route path="/">
-                        <MainView />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <SnackbarProvider
+            maxSnack={1}
+            domRoot={document.getElementById('main-view') || undefined}
+            anchorOrigin={{
+                horizontal: 'center',
+                vertical: 'bottom'
+            }}
+        >
+            <Router history={history}>
+                <div className="App">
+                    <Switch>
+                        <Route path="/login">
+                            <LoginView />
+                        </Route>
+                        <Route path="/">
+                            <MainView />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </SnackbarProvider>
     );
 }
 
