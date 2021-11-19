8e57908295cf8be30f3389de7080f54c7bb5a7d4
Mikko Pirhonen <mikko.m.pirhonen@tuni.fi>
2021-11-19 16:25:15 +0200

    Change notification api

--- frontend/src/hooks/useCreateNotification.ts
+++ frontend/src/hooks/useCreateNotification.ts
@@ -1,20 +1,36 @@
 import { useSnackbar } from 'notistack';
 
-type NotificationDetails = {
-    message: string;
-    type: 'default' | 'error' | 'success' | 'warning' | 'info';
-};
+type NotificationType = 'default' | 'error' | 'success' | 'warning' | 'info';
 
 const useCreateNotification = () => {
     const { enqueueSnackbar } = useSnackbar();
 
-    const createNotification = (notification: NotificationDetails) => {
-        enqueueSnackbar(notification.message, {
-            variant: notification.type
+    const createNotificationWithType = (
+        message: string,
+        type: NotificationType
+    ) => {
+        enqueueSnackbar(message, {
+            variant: type
         });
     };
 
-    return createNotification;
+    const createSuccessNotification = (message: string) => {
+        enqueueSnackbar(message, {
+            variant: 'success'
+        });
+    };
+
+    const createErrorNotification = (message: string) => {
+        enqueueSnackbar(message, {
+            variant: 'error'
+        });
+    };
+
+    return {
+        createSuccessNotification,
+        createErrorNotification,
+        createNotificationWithType
+    };
 };
 
 export default useCreateNotification;
